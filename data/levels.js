568233b4d21ecede5cbf99f35a703c8b3c9e74ac
Mr Footwork <pandau.ting@gmail.com>
2024-11-04 01:43:50 +0100

    spawn 100 asteroids at once

--- data/levels.js
+++ data/levels.js
@@ -1,10 +1,16 @@
 const levels = [
 	{
 		level: 1,
-		initialAsteroids: 1,
-		spawnRate: 2000,
+		initialAsteroids: 100,
+		spawnRate: Infinity,
 		speedMultiplier: 1.0,
 	},
+	// {
+	// 	level: 1,
+	// 	initialAsteroids: 1,
+	// 	spawnRate: 2000,
+	// 	speedMultiplier: 1.0,
+	// },
 	// { level: 1, initialAsteroids: 3, spawnRate: 5000, speedMultiplier: 1.0 },
 	{ level: 2, initialAsteroids: 5, spawnRate: 4000, speedMultiplier: 1.2 },
 	{ level: 3, initialAsteroids: 7, spawnRate: 3000, speedMultiplier: 1.5 },
